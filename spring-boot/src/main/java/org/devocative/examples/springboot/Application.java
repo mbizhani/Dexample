67fb74e7dd0fb66fe6baeba176a9e75daa70b2c2
Mehdi Bizhani <mbizhani@gmail.com>
2018-08-17 22:26:17 +0430

    .update boot example

--- spring-boot/src/main/java/org/devocative/examples/springboot/Application.java
+++ spring-boot/src/main/java/org/devocative/examples/springboot/Application.java
@@ -1,12 +1,30 @@
 package org.devocative.examples.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println(">>> args = " + Arrays.toString(args));
+			System.out.println(">>> #BEANS = " + ctx.getBeanDefinitionCount());
+
+			Arrays
+				.stream(ctx.getBeanDefinitionNames())
+				.sorted()
+				.forEach(s -> System.out.println(">>> Bean: " + s));
+		};
 	}
 }
