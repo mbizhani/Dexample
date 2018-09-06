864ac30018468b13d7564b84eff97f2607cc65a5
Mehdi Bizhani <mbizhani@gmail.com>
2018-09-06 19:19:50 +0430

    final config

--- docker/openvpn-tinyproxy/config/opt/vpnbook/run.sh
+++ docker/openvpn-tinyproxy/config/opt/vpnbook/run.sh
@@ -1,7 +1,9 @@
-#! /bin/bash
+#!/bin/bash
+
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 /usr/sbin/tinyproxy
 
 echo -e "vpnbook\n${VPN_PASSWD}" > /opt/vpnbook/realm.txt
 
-/usr/sbin/openvpn --config /opt/vpnbook/books/vpnbook-fr1-tcp80.ovpn --auth-user-pass /opt/vpnbook/realm.txt
+/usr/sbin/openvpn --config /opt/vpnbook/books/${VPN_BOOK} --auth-user-pass /opt/vpnbook/realm.txt
