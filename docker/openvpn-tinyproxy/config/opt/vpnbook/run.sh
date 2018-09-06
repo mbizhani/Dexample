5d82c34d08248e0ae53b5c509206e69626f181ee
Mehdi Bizhani <mbizhani@gmail.com>
2018-09-07 01:32:33 +0430

    no need for --device

--- docker/openvpn-tinyproxy/config/opt/vpnbook/run.sh
+++ docker/openvpn-tinyproxy/config/opt/vpnbook/run.sh
@@ -6,4 +6,9 @@ echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 echo -e "vpnbook\n${VPN_PASSWD}" > /opt/vpnbook/realm.txt
 
+mkdir -p /dev/net
+if [ ! -c /dev/net/tun ]; then
+  mknod /dev/net/tun c 10 200
+fi
+
 /usr/sbin/openvpn --config /opt/vpnbook/books/${VPN_BOOK} --auth-user-pass /opt/vpnbook/realm.txt
