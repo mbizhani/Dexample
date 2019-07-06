e6e091eddbe0ca6f0c21348b0cfdb2cfd14d6f75
Mehdi Bizhani <mbizhani@gmail.com>
2019-07-07 00:11:29 +0430

    webpack-dev-server

--- reactjs/webpack.config.js
+++ reactjs/webpack.config.js
@@ -1,10 +1,13 @@
+const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   "mode": "development",
+  "context": path.join(__dirname, "src"),
   "entry": "./index.js",
   "output": {
-    "path": __dirname,
+    "path": path.resolve(__dirname, "dist"),
     "filename": "bundle.js"
   },
   "module": {
@@ -14,5 +17,11 @@ module.exports = {
         "loader": "babel-loader"
       }
     ]
-  }
+  },
+  "plugins": [
+    new HtmlWebpackPlugin({
+      "template": "index.html",
+      "inject": "body"
+    }) 
+  ]
 };
