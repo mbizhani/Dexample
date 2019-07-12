505f1668b284112098a2788dfd42b8e520124806
Mehdi Bizhani <mehdi@devocative.devocative.org>
2019-07-12 21:43:37 +0430

    functional component, add image

--- reactjs/webpack.config.js
+++ reactjs/webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
       {
         "test": /\.js$/,
         "loader": "babel-loader"
+      },
+      {
+        "test": /\.(png|jpeg)$/,
+        "loader": "file-loader"
       }
     ]
   },
